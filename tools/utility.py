bc066e7c853168ff07625ceff99eb7758efb9eb3
Marcus DuPont <dupontdev@gmail.com>
2022-04-28 20:26:54 -0400

    move around some utlity funcs and add Scale class

--- tools/utility.py
+++ tools/utility.py
@@ -10,24 +10,33 @@ import argparse
 import matplotlib.pyplot as plt 
     
 from typing import Union
+from typing import Callable
 
+c = const.c.cgs
 # FONT SIZES
-SMALL_SIZE   = 6
-DEFAULT_SIZE = 10
-BIGGER_SIZE  = 12
+class Scale:
+    def __init__(self, scale='blandford_mckee'):
+        if scale == 'hydro':
+            #================================
+            #   Hydro scales
+            #================================
+            self.length_scale  = const.R_sun.cgs 
+            self.m_sun         = const.M_sun.cgs
+            self.rho_scale     = self.m_sun / (4./3. * np.pi * self.length_scale ** 3) 
+            self.e_scale       = self.m_sun * c **2
+            self.pre_scale     = self.e_scale / (4./3. * np.pi * self.length_scale**3)
+            self.time_scale    = self.length_scale / c
+        elif scale == 'blandford_mckee':
+            #==============
+            # BMK Scales
+            #==============
+            self.e_scale      = 1e53 * units.erg 
+            self.rho_scale    = 1.0 * const.m_p.cgs / units.cm**3 
+            self.length_scale = ((self.e_scale / (self.rho_scale * const.c.cgs**2))**(1/3)).to(units.cm)
+            self.time_scale   = self.length_scale / const.c.cgs 
+            self.pre_scale    = self.e_scale / self.length_scale ** 3  
 
-#================================
-#   constants of nature
-#================================
-R_0 = const.R_sun.cgs 
-c   = const.c.cgs
-m   = const.M_sun.cgs
- 
-rho_scale  = m / (4./3. * np.pi * R_0 ** 3) 
-e_scale    = m * c **2
-pre_scale  = e_scale / (4./3. * np.pi * R_0**3)
-time_scale = R_0 / c
-
+scales = Scale()
 
 def calc_rverticies(r: np.ndarray) -> np.ndarray:
     rvertices = np.sqrt(r[1:] * r[:-1])
@@ -348,7 +357,7 @@ def read_1d_file(filename: str) -> dict:
         
         a    = (4 * const.sigma_sb.cgs / c)
         k    = const.k_B.cgs
-        T    = (3 * p * pre_scale  / a)**(1./4.)
+        T    = (3 * p * scales.pre_scale  / a)**(1./4.)
         T_eV = (k * T).to(units.eV)
         
         h = 1.0 + 4/3 * p / (rho * (4/3 - 1))
@@ -396,7 +405,7 @@ def prims2var(fields: dict, var: str) -> np.ndarray:
         return fields['rho']*h*W**2 - fields['p']
     elif var == 'temperature':
         a    = (4.0 * const.sigma_sb.cgs / c)
-        T    = (3.0 * fields['p'] * pre_scale  / a)**0.25
+        T    = (3.0 * fields['p'] * scales.pre_scale  / a)**0.25
         T_eV = (const.k_B.cgs * T).to(units.eV)
         return T_eV.value
     elif var == 'gamma_beta':
@@ -418,8 +427,8 @@ def read_example_afterglow_data(filename: str) -> dict:
     with h5py.File(filename, "r") as hf:
         nu   = hf.get('nu')[:]   * units.Hz
         t    = hf.get('t')[:]    * units.s 
-        fnu  = hf.get('fnu')[:]  * 1e26 * units.Jy 
-        fnu2 = hf.get('fnu2')[:] * 1e26 * units.Jy 
+        fnu  = hf.get('fnu')[:]  * 1e26 * units.mJy 
+        fnu2 = hf.get('fnu2')[:] * 1e26 * units.mJy 
         
     tday = t.to(units.day)
     data_dict = {}
@@ -432,6 +441,25 @@ def read_example_afterglow_data(filename: str) -> dict:
     
     return data_dict
 
+def read_my_datafile(filename: str) -> dict:
+    """
+    Reads afterglow data from afterglow library (Zhang and MacFadyen 2009 or van Eerten et al. 2010)
+    """
+    with h5py.File(filename, "r") as hf:
+        nu   = hf.get('nu')[:]    * units.Hz
+        tday = hf.get('tbins')[:] * units.day
+        fnu  = hf.get('fnu')[:]   * units.mJy 
+        
+
+    data_dict = {}
+    data_dict['tday'] = tday 
+    data_dict['freq'] = nu 
+    data_dict['fnu'] = {nu_val: fnu[i, :] for i, nu_val in enumerate(nu)}
+    # data_dict['light_curve_pcj'] = {nu_val: fnu[i, :] + fnu2[i, :] for i, nu_val in enumerate(nu)}
+    # data_dict['spectra'] = {tday_val: fnu[:, i] for i, tday_val in enumerate(tday)}
+    # data_dict['spectra_pcj'] = {tday_val: fnu2[:, i] for i, tday_val in enumerate(tday)}
+    
+    return data_dict
 def get_colors(interval: np.ndarray, cmap: plt.cm, vmin: float = None, vmax: float = None):
     """
     Return array of rgba colors for a given matplotlib colormap
@@ -460,3 +488,374 @@ def find_nearest(arr: list, val: float) -> int:
         x   = np.abs(arr - val)
         idx = np.where(x == x.min())
         return idx 
+    
+
+def generate_mesh(args: argparse.ArgumentParser, mesh: dict):
+    """
+    Generate a real or pseudo 3D mesh based on checkpoint data
+    
+    Parameters
+    --------------------------
+    args: argparser arguments from CLI 
+    mesh: the mesh data from the checkpoint
+    
+    Return
+    --------------------------
+    rr:      (np.ndarray) 3D array of radial points
+    thetta:  (np.ndarray) 3D array of polar points
+    phii:    (np.ndarray) 3D arrray of azimuthal points
+    ndim:    (int)        number of dimension from checkpoint mesh
+    """
+    
+    r = mesh['r']
+    ndim = 1
+    if 'theta' in mesh:
+        ndim += 1
+        theta = mesh['theta']
+    else:
+        dlogr         = np.log10(r.max()/r.min())/(r.size - 1)
+        theta_max     = np.pi / 2 if not args.full_sphere else np.pi
+        theta_samples = int(theta_max / dlogr + 1) if not args.theta_samples else args.theta_samples 
+        theta         = np.linspace(0.0, theta_max, theta_samples)
+        
+    if 'phi' in mesh:
+        ndim += 1
+        phi  = mesh['phi']
+    else:
+        phi = np.linspace(0.0, 2.0 * np.pi, args.phi_samples)
+
+    thetta, phii, rr = np.meshgrid(theta, phi, r)
+    
+    return rr, thetta, phii, ndim
+
+def get_tbin_edges(
+    args: argparse.ArgumentParser, 
+    file_reader: Callable,
+    files: str):
+    """
+    Get the bin edges of the lightcurves based on the checkpoints given
+    
+    Parameters:
+    -----------------------------------
+    files: list of files to draw data from
+    
+    Returns:
+    -----------------------------------
+    tmin, tmax: tuple of time bins in units of days
+    """
+    
+    setup_init,  mesh_init  = file_reader(files[0])[1:]
+    setup_final, mesh_final = file_reader(files[-1])[1:]
+    
+    t_beg = setup_init['time']  * scales.time_scale
+    t_end = setup_final['time'] * scales.time_scale
+    
+    rr0, thetta, phii = generate_mesh(args, mesh_init)[:-1]
+    rrf               = generate_mesh(args, mesh_final)[0]
+    rhat              = np.array([np.sin(thetta)*np.cos(phii), np.sin(thetta)*np.sin(phii), np.cos(thetta)])  # radial unit vector                                                                                 # electron particle number index   
+    
+    # Place observer along chosen axis
+    theta_obs_rad = np.deg2rad(args.theta_obs)
+    theta_obs     = theta_obs_rad * np.ones_like(thetta)
+    obs_hat       = np.array([np.sin(theta_obs), np.zeros_like(thetta), np.cos(theta_obs)])
+    r_dot_nhat    = vector_dotproduct(rhat, obs_hat)
+    
+        
+    t_obs_min  = t_beg - rr0 * scales.time_scale * r_dot_nhat
+    t_obs_max  = t_end - rrf * scales.time_scale * r_dot_nhat
+
+    theta_idx  = find_nearest(thetta[0,:,0], theta_obs_rad)[0]
+    t_obs_beam = t_obs_min[0][theta_idx]
+    t_obs_beam = t_obs_beam[t_obs_beam >= 0]
+    
+    tmin       = (t_obs_beam.min()).to(units.day)
+    tmax       = (t_obs_max[t_obs_max > 0].max()).to(units.day)
+    
+    return tmin, tmax
+
+def running_mean(x, N):
+    cumsum = np.cumsum(np.insert(x, 0, 0)) 
+    return (cumsum[N:] - cumsum[:-N]) / float(N)
+
+def shock_finder(vfield: np.ndarray, r: np.ndarray) -> int:
+    """
+    Computes the divergence of the vector field vfield, corresponding to dFx/dx + dFy/dy + ...
+    :return: index where divergence becomes negative
+    """
+    def first_neg(arr: np.ndarray):
+        idx = 0
+        for number in arr:
+            idx += 1 
+            if number < 0:
+                return idx
+    
+    n = 5
+    sections = vfield.size // n
+    num_dims = np.ndim(vfield)
+    divs     = []
+    for nth in range(sections):
+        edge  = nth + n if nth + 5 < vfield.size - 1 else  vfield.size - 1
+        div_v = 1/r[nth:edge]**2 * np.ufunc.reduce(np.add, np.gradient(r[nth:edge]**2*vfield[nth:edge], r[nth:edge]))
+        divs += [div_v]
+    
+    divs   = np.asanyarray(divs)
+
+def calc_bfield_shock(einternal: float, eps_b: float) -> float:
+    """
+    Calc magnitude of magnetic field assuming shock jump conditions
+    
+    Params:
+    ------------------------------------------------
+    einternal:     internal energy density
+    eps_b:            magnetic energy fraction
+    
+    Return:
+    ------------------------------------------------
+    The magnetic field magnitude 
+    """
+    return (8.0 * np.pi * eps_b * einternal)**0.5
+    
+def calc_gyration_frequency(b_field: float) -> float:
+    """
+    return gyration frequency in units of MHz
+    Params
+    ---------------------
+    b_field: magnetic field value 
+    
+    Return
+    ---------------------
+    the gyration frequency 
+    """
+    frequency_for_unit_field = (3.0 / 4.0 / np.pi) * (const.e.gauss * 1.0 * units.gauss) / (const.m_e.cgs * const.c.cgs)
+    return frequency_for_unit_field.value  * b_field.value * units.Hz
+
+def calc_total_synch_power(lorentz_gamma: float, ub: float, beta: float) -> float:
+    """
+    Calc bolometric synhrotron power
+    
+    Params:
+    --------------------------------------
+    lorentz_gamma:   lorentz factor 
+    ub:              magnetic field energy density 
+    beta:            dimensionless flow veloccity 
+    
+    Return:
+    --------------------------------------
+    Total synchrotron power
+    """
+    return (4.0/3.0) * const.sigma_T.cgs * beta**2 * lorentz_gamma ** 2 * const.c.cgs * ub
+
+def calc_nphotons_per_bin(volume: float, n_e: float, nu_g:float, gamma_e: float, beta: float, u_b: float, dt: float, p: float = 2.5) -> float:
+    """
+    Calculate the number of photons per energy (gamma_e) bin
+    
+    Params:
+    ------------------------------------------
+    volume:              volume of the cell
+    n_e:                 number density of electrons
+    nu_g:                gyration frequency of electrons
+    gamma_e:             lorentz factor of electrons
+    u_b:                 magnetic field energy density 
+    dt:                  the checkpoint time interval 
+    p:                   electron spectral index 
+    
+    Return:
+    ------------------------------------------
+    number of photons per energy bin
+    """
+    a = (8.0 * np.pi * volume / (3.0 * const.h.cgs * nu_g))
+    b = const.sigma_T.cgs * const.c.cgs * beta**2 * u_b * n_e 
+    c = gamma_e**(-(p + 1.0))
+    return a * b * c * dt
+
+# def calc_nphotons_per_electron(volume: float, n_e: float, nu_g:float, nu_c: float, beta: float, u_b: float, dt: float, p: float = 2.5) -> float:
+#     """
+#     Calculate the number of photons in energy bin
+    
+#     Params:
+#     ------------------------------------------
+#     volume:              volume of the cell
+#     n_e:                 number density of electrons
+#     nu_g:                gyration frequency of electrons
+#     nu_c:                critical frequency of photons
+#     beta:                dimensionless velocity wrt to speed of light
+#     u_b:                 magnetic field energy density 
+#     dt:                  the checkpoint time interval 
+#     p:                   electron spectral index 
+    
+#     Return:
+#     ------------------------------------------
+#     number of photons per energy bin
+#     """
+#     a = (16.0 * np.pi * volume / (3.0 * const.h.cgs * nu_g * (p - 1)))
+#     b = const.sigma_T.cgs * const.c.cgs * beta**2 * u_b * n_e 
+#     c = (nu_c/nu_g)**(-(p - 1.0)/2.0)
+#     return a * b * c * dt
+
+def calc_nphotons_other(power: float, nu_c: float, dt:float):
+    """
+    Calculate the number of photons per electron in energy bin
+    
+    Params: 
+    -------------------------------------
+    power:                 the total synchrotron power per electron
+    nu_c:                  the critical frequency of electrons
+    dt:                    the time step size between checkpoints
+    
+    Return:
+    -------------------------------------
+    calculate the number of photons emitted per electron in some energy bin
+    """
+    return power * dt / (const.h.cgs * nu_c)
+
+def gen_random_from_powerlaw(a, b, g, size=1):
+    """Power-law generator for pdf(x) ~ x^{g-1} for a<=x<=b"""
+    rng = np.random.default_rng()
+    r = rng.random(size=size)[0]
+    ag, bg = a**g, b**g
+    return (ag + (bg - ag)*r)**(1.0/g)
+
+def vector_magnitude(a: np.ndarray) -> np.ndarray:
+    """return magnitude of vector(s)"""
+    if a.ndim <= 3:
+        return (a.dot(a))**0.5
+    else:
+        return (a[0]**2 + a[1]**2 + a[2]**2)**0.5
+        
+def vector_dotproduct(a: np.ndarray, b: np.ndarray) -> float:
+    """dot product between vectors or array of vectors"""
+    if a.ndim <= 3:
+        return a.dot(b)
+    else:
+        return a[0]*b[0] + a[1]*b[1] + a[2]*b[2]
+
+def calc_nelectrons_at_gamma_e(n_e: float, gamma_e: float, p: float = 2.5) -> float:
+    """
+    Calculate the number density of electrons per lorentz facor 
+    Params:
+    --------------------------------------
+    n_e:                       total number density of electrons
+    gamma_e:                   the electron lorentz factor
+    dgamma:                    the energy bin size 
+    p:                         electron spectral index 
+    
+    Return:
+    -------------------------------------
+    number density of electrons at a given energy
+    """
+    return n_e * gamma_e ** (-p)
+
+def calc_doppler_delta(lorentz_gamma: float, beta_vector: np.ndarray, n_hat: np.ndarray) -> np.ndarray:
+    """
+    Calculate the dpppler factor given lorentz factor, velocity vector, and propagation vector
+    
+    Params:
+    --------------------------------------
+    lorentz_gamma:           lorentz factor of flow 
+    beta_vector:             velocity vector of flow 
+    n_hat:                   direction of emission
+    
+    Return:
+    ---------------------------------------
+    the standard doppler factor delta 
+    """
+    return 1.0 / (lorentz_gamma * (1.0  - vector_dotproduct(beta_vector, n_hat)))
+
+def calc_nu(gamma_e: float, nu_g: float):
+    """Calculate the synchrotron frequency as function of lorentz_factor"""
+    return gamma_e ** 2 * nu_g 
+
+def calc_critical_lorentz(bfield: float, time: float)-> float:
+    """Calculate the critical Lorentz factor as function of time and magnetic field"""
+    return (6.0 * np.pi * const.m_e.cgs * const.c.cgs) / (const.sigma_T.cgs * bfield ** 2 * time)
+
+def calc_max_power_per_frequency(bfield: float) -> float:
+    """Calculate the maximum power per frequency""" 
+    return (const.m_e.cgs * const.c.cgs ** 2 * const.sigma_T.cgs) / (3.0 * const.e.gauss) * bfield
+
+def calc_emissivity(bfield: float, n: float, p: float) -> float:
+    """Calculate the peak emissivity per frequency per equation (A3) in
+    https://iopscience.iop.org/article/10.1088/0004-637X/749/1/44/pdf
+    """ 
+    eps_m = (9.6323/ 8.0 / np.pi) * (p - 1.0) / (3.0 * p - 1.0) * (3.0)**0.5 * const.e.gauss**3 / (const.m_e.cgs * const.c.cgs**2) * n * bfield
+    return eps_m
+
+def calc_minimum_lorentz(eps_e: float,e_thermal: float, n: float, p: float) -> float:
+    """
+    Calculate the minimum lorentz factor of electrons in the distribution
+    
+    Params:
+    ------------------------------
+    eps_e:              fraction of internal energy due to shocked electrons
+    p:                  spectral electron number index 
+    
+    Return:
+    ------------------------------
+    Minimum lorentz factor of electrons
+    """
+    return eps_e * (p - 2.0) / (p - 1.0) * e_thermal / (n * const.m_e.cgs * const.c.cgs**2)
+
+def calc_powerlaw_flux(
+    mesh:     dict,
+    flux_max: float, 
+    p:        float,
+    nu:       float, 
+    nu_c:     float, 
+    nu_m:     float, 
+    ndim:     int = 1) -> float:
+    """
+    ---------------------------------------
+    Compute the flux according to https://arxiv.org/abs/astro-ph/9712005
+    ---------------------------------------
+    """
+    f_nu = flux_max.copy()
+    slow_cool    = nu_c > nu_m
+    fast_cool    = nu_c < nu_m
+    
+    fast_break1  = nu_c > nu 
+    fast_break2  = nu   > nu_m
+    fast_mask1   = fast_cool & fast_break1 
+    fast_mask2   = fast_cool & fast_break2
+    fast_mask3   = fast_cool & (fast_break1 == False) & (fast_break2 == False)
+    
+    slow_break1  = nu_m > nu 
+    slow_break2  = nu   > nu_c
+    slow_mask1   = slow_cool & slow_break1
+    slow_mask2   = slow_cool & slow_break2
+    slow_mask3   = slow_cool & (slow_break1 == False) & (slow_break2 == False)
+    
+    if ndim == 1:
+        # Collapse the masks into their respective 1D symmetries
+        slow_mask1 = slow_mask1[0][0]
+        slow_mask2 = slow_mask2[0][0]
+        slow_mask3 = slow_mask3[0][0]
+        
+        fast_mask1 = fast_mask1[0][0]
+        fast_mask2 = fast_mask2[0][0]
+        fast_mask3 = fast_mask3[0][0]
+        
+        f_nu[:, :, slow_mask1] *= (nu[:, :, slow_mask1] / nu_m[slow_mask1])**(1.0 / 3.0)  
+        f_nu[:, :, slow_mask2] *= (nu_c[slow_mask2]     / nu_m[slow_mask2])**(-0.5 * (p - 1.0)) * (nu[:, :, slow_mask2] / nu_c[slow_mask2])**(-0.5 * p)
+        f_nu[:, :, slow_mask3] *= (nu[:, :, slow_mask3] / nu_m[slow_mask3])**(-0.5 * (p - 1.0))
+        
+        f_nu[:, :, fast_mask1] *= (nu[:, :, fast_mask1] / nu_c[fast_mask1])**(1.0 / 3.0)
+        f_nu[:, :, fast_mask2] *= (nu_m[fast_mask2]     / nu_c[fast_mask2])**(-0.5) * (nu[:, :, fast_mask2] / nu_m[fast_mask2])**(-0.5 * p)
+        f_nu[:, :, fast_mask3] *= (nu[:, :, fast_mask3] / nu_c[fast_mask3])**(-0.5)
+    else:
+        # Collapse the masks into their respective 2D symmetries
+        slow_mask1 = slow_mask1[0]
+        slow_mask2 = slow_mask2[0]
+        slow_mask3 = slow_mask3[0]
+        
+        fast_mask1 = fast_mask1[0]
+        fast_mask2 = fast_mask2[0]
+        fast_mask3 = fast_mask3[0]
+        f_nu[:, slow_mask1] *= (nu[:, slow_mask1] / nu_m[slow_mask1])**(1.0 / 3.0)  
+        f_nu[:, slow_mask2] *= (nu_c[slow_mask2]  / nu_m[slow_mask2])**(-0.5 * (p - 1.0))*(nu[:, slow_mask2] / nu_c[slow_mask2])**(-0.5 * p)
+        f_nu[:, slow_mask3] *= (nu[:, slow_mask3] / nu_m[slow_mask3])**(-0.5 * (p - 1.0))
+        
+        f_nu[:, fast_mask1] *= (nu[:, fast_mask1] / nu_c[fast_mask1])**(1.0 / 3.0)
+        f_nu[:, fast_mask2] *= (nu_m[fast_mask2]  / nu_c[fast_mask2])**(-0.5)*(nu[:, fast_mask2] / nu_m[fast_mask2])**(-0.5 * p)
+        f_nu[:, fast_mask3] *= (nu[:, fast_mask3] / nu_c[fast_mask3])**(-0.5)
+        
+    return f_nu
